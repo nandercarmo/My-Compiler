b1b59bab9f8c0394ed4d578ae6974f85bb487cbb
NanderSantos <nandermgsdp@gmail.com>
2021-05-28 10:41:06 -0300

    Correçao do arquivo install.sh.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo -e "Moving file..."
 sudo cp src/compile /usr/bin
 echo -e "Entering the compile folder..."
 cd /usr/bin
-echo -e "Granting execution permissions"
+echo -e "Granting execution permissions..."
 sudo chmod +x compile
 echo -e "Done!\n"
 cd
